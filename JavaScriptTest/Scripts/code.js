9646b275f4e7f085a0d16b85d6d27f541e7a8a06
JeremieBouG <Jeremie.Boudreau2@canada.ca>
2016-03-11 14:25:03 -0500

    it should work

--- JavaScriptTest/Scripts/code.js
+++ JavaScriptTest/Scripts/code.js
@@ -3,23 +3,6 @@ var redirectUri = "http://crcpowerbitest.azurewebsites.net/javaScriptTest/";
 function getPowerBICallback(error, token){
   console.log("error: " + error );
   console.log("token: " + token);
-
-  if(token != null){
-    var request = new XMLHttpRequest();
-
-    request.open('GET', 'https://api.powerbi.com/v1.0/myorg/datasets');
-    request.setRequestHeader("Authorization", "Bearer " + token);
-
-    request.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        console.log('Status:', this.status);
-        console.log('Headers:', this.getAllResponseHeaders());
-        console.log('Body:', this.responseText);
-      }
-    };
-
-    request.send();
-  }
 }
 
 
@@ -28,9 +11,10 @@ function getPowerBI()
   var app = angular.module('demoApp', ['ngRoute', 'AdalAngular'])
 
   var url = window.location.href
-
+  console.log(url);
+  console.log(url.length);
   if(url.length != redirectUri.length){
-    console.log("hello");
+    var token = getQueryVariable("id_token");
   }
 
   // using '!' as the hashPrefix but can be a character of your choosing
@@ -52,7 +36,39 @@ function getPowerBI()
   authContext.login();
   authContext.acquireToken ("https://analysis.windows.net/powerbi/api", getPowerBICallback);
   console.log("got here");
+}
 
+function getQueryVariable(variable) {
+    var query = window.location.hash.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
 
+function callPowerBIRest(token){
+    if(token = null){
+        console.log("The token is null");
+        return;
+    }
+    else{    
+      var request = new XMLHttpRequest();
 
+      request.open('GET', 'https://api.powerbi.com/v1.0/myorg/datasets');
+      request.setRequestHeader("Authorization", "Bearer " + token);
+
+      request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          console.log('Status:', this.status);
+          console.log('Headers:', this.getAllResponseHeaders());
+          console.log('Body:', this.responseText);
+        }
+      };
+
+      request.send();
+    }
 }
